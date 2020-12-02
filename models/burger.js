549953400d2f00f8a820f2b251ef3ead13fc6b87
James Baldwin <jamescbaldwin@gmail.com>
2020-12-02 15:00:57 -0500

    model and controller framework

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,25 @@
 //import orm.js
 //call ORM functions using burger specific input for the orm
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        })
+    };
+};
+
+module.exports = burger;
