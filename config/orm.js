263abfd84328ce175c144c40da47e521f3948dd1
James Baldwin <jamescbaldwin@gmail.com>
2020-12-02 22:46:33 -0500

    daily aggregate commit

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,28 @@
-//import(require) connection.js into orm.js
-//requisite methods: selectAll(), insertOne(), updateOne()
-//export ORM object 
+var connection = require("./connection.js");
+
+var orm = {
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO ?? (??) VALUES (?)";
+    connection.query(queryString, [table, cols, vals], function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  updateOne: function(table, cols, updatedValue, id, cb) {
+    var queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
+    connection.query(queryString, [table, cols, updatedValue, id], function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
